90679523288a517fe17bb610294dcaed4e196891
BigOzzyOz <60309436+BigOzzyOz@users.noreply.github.com>
2025-03-16 17:47:48 +0100

    cleanCode html and add environments to .gitignore

--- assets/environments/firebase-config.js
+++
@@ -1,9 +0,0 @@
-export const firebaseConfig = {
-  apiKey: "AIzaSyAVuunbXTGr2CljBpc6fitx4--oaLEfEsE",
-  authDomain: "join-273.firebaseapp.com",
-  databaseURL: "https://join-273-default-rtdb.europe-west1.firebasedatabase.app",
-  projectId: "join-273",
-  storageBucket: "join-273.firebasestorage.app",
-  messagingSenderId: "180936491276",
-  appId: "1:180936491276:web:9d04962cf61d639e138929"
-};
