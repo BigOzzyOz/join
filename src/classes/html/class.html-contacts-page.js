c0d8de9150ea07b938834ec427bfc593c8af3dd0
BigOzzyOz <60309436+BigOzzyOz@users.noreply.github.com>
2025-04-15 16:30:12 +0200

    refactor: enhance HTML rendering methods with improved safety and clarity

--- src/classes/html/class.html-contacts-page.js
+++ src/classes/html/class.html-contacts-page.js
@@ -1,38 +1,45 @@
 export class ContactsPageHtml {
-    constructor(kanban) {
-        this.kanban = kanban;
-    }
+  //NOTE - Properties
 
-    htmlRenderContactDetailsEmpty() {
-        return /*html*/`
-          <div id="contactsDetail" class="ttx100">
+  kanban;
+
+  //NOTE - Constructor & Initialization
+  constructor(kanban) {
+    this.kanban = kanban;
+  }
+
+  //NOTE - HTML Rendering Methods
+
+  htmlRenderContactDetailsEmpty() {
+    return /*html*/`
+          <div id="contactsDetail" class="ttx100"> 
             <div class="contactsHeader">
               <h1>Contacts</h1>
               <p class="bordered fs20">Better with a Team</p>
             </div>
           </div>
           `;
-    }
+  }
 
-
-    htmlRenderAddContact() {
-        return /*html*/`
+  htmlRenderAddContact() {
+    return /*html*/`
           <div id="moreContactsButton" class="moreIcon">
             <p>Add new contact</p>
-            <img src="../assets/icons/person_add.svg" alt="person add">
+            <img src="../assets/icons/person_add.svg" alt="Add new contact">
           </div>
           `;
-    }
+  }
 
 
+  htmlRenderContactLetter(letter) {
+    const safeLetter = letter.replace(/</g, "&lt;").replace(/>/g, "&gt;");
 
-    htmlRenderContactLetter(letter) {
-        return /*html*/`
+    return /*html*/`
           <div class="contactLetter">
-            <p class="fs20">${letter}</p>
-            <ul id="contactLetter${letter}" class="contactList">
+            <p class="fs20">${safeLetter}</p>
+            <ul id="contactLetter${safeLetter}" class="contactList">
             </ul>
           </div>
         `;
-    }
+  }
 }
