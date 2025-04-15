a762f64e6a8cc71076bd0a10c9d3a01ce02e43ba
BigOzzyOz <60309436+BigOzzyOz@users.noreply.github.com>
2025-04-15 13:15:36 +0200

    refactor: enhance ContactHtml class with improved data handling and safe HTML rendering

--- src/classes/html/class.html-contact.js
+++ src/classes/html/class.html-contact.js
@@ -1,4 +1,16 @@
 export class ContactHtml {
+  //NOTE - Properties
+
+  id;
+  email;
+  firstLetters;
+  isUser;
+  name;
+  profilePic;
+  phone;
+
+  //NOTE - Constructor & Initialization
+
   constructor(contact) {
     this.id = contact.id;
     this.email = contact.email;
@@ -6,16 +18,25 @@ export class ContactHtml {
     this.isUser = contact.isUser;
     this.name = contact.name;
     this.profilePic = contact.profilePic;
-    this.phone = contact.number;
+    this.phone = contact.phone;
   }
 
+  //NOTE - HTML Rendering Methods
+
   htmlRenderContactDetails() {
-    return /*html*/`
+    const safeName = this.name.replace(/</g, "&lt;").replace(/>/g, "&gt;");
+    const safeEmail = this.email.replace(/</g, "&lt;").replace(/>/g, "&gt;");
+    const safePhone = this.phone.replace(/</g, "&lt;").replace(/>/g, "&gt;");
+
+    const mailtoLink = this.email ? `mailto:${encodeURIComponent(this.email)}` : '#';
+    const telLink = this.phone ? `tel:${encodeURIComponent(this.phone)}` : '#';
+
+    return /*html*/ `
       <div id="contactsDetailMore" class="moreIcon">
-        <img src="../assets/icons/more_vert.svg" alt="3 points vert">
+        <img src="../assets/icons/more_vert.svg" alt="More options">
       </div>
       <a id="backArrow-contacts" class="backArrow">
-        <img src="../assets/icons/arrow-left-line.svg" alt="arrow left line">
+        <img src="../assets/icons/arrow-left-line.svg" alt="Back">
       </a>
       <div class="contactsHeader">
         <h1>Contacts</h1>
@@ -23,19 +44,19 @@ export class ContactHtml {
       </div>
       <div class="contactOverview">
         <div class="contactBig">
-          ${this.profilePic}
+          ${this.profilePic || `<div class="initials-placeholder">${this.firstLetters}</div>`}
         </div>
         <div>
-          <h2>${this.name}</h2>
+          <h2>${safeName}</h2>
           <div id="editMenu" class="editMenu ts0">
             <div id="editContactBtn" class="editMenuItem" data-id="${this.id}">
-              <img class="editMenuButton" src="../assets/icons/edit.svg" alt="pencil">
-              <img class="editMenuButton hoverEffectIcon" src="../assets/icons/editBlue.svg" alt="blue pencil">
+              <img class="editMenuButton" src="../assets/icons/edit.svg" alt="Edit">
+              <img class="editMenuButton hoverEffectIcon" src="../assets/icons/editBlue.svg" alt="">
               <p>Edit</p>
             </div>
             <div id="deleteContactBtn" class="editMenuItem" data-id="${this.id}">
-              <img class="editMenuButton" src="../assets/icons/delete.svg" alt="trashcan">
-              <img class="editMenuButton hoverEffectIcon" src="../assets/icons/deleteBlue.svg" alt="blue trashcan">
+              <img class="editMenuButton" src="../assets/icons/delete.svg" alt="Delete">
+              <img class="editMenuButton hoverEffectIcon" src="../assets/icons/deleteBlue.svg" alt="">
               <p>Delete</p>
             </div>
           </div>
@@ -43,24 +64,28 @@ export class ContactHtml {
       </div>
       <p class="fs20">Contact Information</p>
       <h4>Email</h4>
-      <p><a href="${this.email}">${this.email}</a></p>
+      <p><a href="${mailtoLink}">${safeEmail || 'No email provided'}</a></p>
       <h4>Phone</h4>
-      <p><a href="${this.phone}">${this.phone}</a></p>
+      <p><a href="${telLink}">${safePhone || 'No phone provided'}</a></p>
       `;
   }
 
   htmlRenderGeneral(currentUserId) {
-    return /*html*/`
+    const safeName = this.name.replace(/</g, "&lt;").replace(/>/g, "&gt;");
+    const safeEmail = this.email.replace(/</g, "&lt;").replace(/>/g, "&gt;");
+
+    const mailtoLink = this.email ? `mailto:${encodeURIComponent(this.email)}` : '#';
+
+    return /*html*/ `
       <li id="contact${this.id}" class="contactListItem" data-id="${this.id}">
         <div class="contactSmall">
-          ${this.profilePic}
+          ${this.profilePic || `<div class="initials-placeholder">${this.firstLetters}</div>`}
         </div>
         <div>
-          <p class="fs20">${this.name} ${this.id === currentUserId ? '(you)' : ''}</p>
-          <p><a>${this.email}</a></p>
+          <p class="fs20">${safeName} ${this.id === currentUserId ? '(you)' : ''}</p>
+          <p><a>${safeEmail || 'No email provided'}</a></p>
         </div>
       </li>
     `;
   }
-
 }
