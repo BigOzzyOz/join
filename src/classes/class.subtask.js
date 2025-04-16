4eede439f3b3b832ea807f02cfce1d8eecdd9c8c
BigOzzyOz <60309436+BigOzzyOz@users.noreply.github.com>
2025-04-16 14:10:47 +0200

    Revert "refactor: Organize functions into dedicated classes"
    This reverts commit 916fa849b0f985f10140b3991f92b23386672953.

--- src/classes/class.subtask.js
+++
@@ -1,6 +0,0 @@
-class Subtask {
-    // Currently no functions identified that belong exclusively to the logic of a single subtask object.
-    // Logic related to managing subtasks during task creation/editing will be in AddTask.
-    // Logic related to updating subtask status within a saved task is in Task.
-    // HTML templating for subtasks will be in HtmlSubtask.
-}
